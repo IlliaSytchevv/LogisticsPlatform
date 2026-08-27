939b71fdfe8aaca48a0ea3c34b37577f3eeea521
Illia <illiasychov@gmail.com>
2026-08-27 12:27:39 +0300

    fix

--- ReversProxy/OcelotGateway/Program.cs
+++ ReversProxy/OcelotGateway/Program.cs
@@ -2,7 +2,6 @@ using AspNetCoreRateLimit;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Polly;
-using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,22 +16,11 @@ else
         .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 }
 
-var redisConnection =
-    builder.Configuration["Redis:ConnectionString"]
-    ?? "localhost:6379,abortConnect=false";
-
-builder.Services.AddSingleton<IConnectionMultiplexer>(_ =>
-    ConnectionMultiplexer.Connect(redisConnection));
-
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    options.Configuration = redisConnection;
-});
-
+builder.Services.AddMemoryCache();
 builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
-builder.Services.AddSingleton<IIpPolicyStore, DistributedCacheIpPolicyStore>();
-builder.Services.AddSingleton<IRateLimitCounterStore, DistributedCacheRateLimitCounterStore>();
-builder.Services.AddSingleton<IClientPolicyStore, DistributedCacheClientPolicyStore>();
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddSingleton<IClientPolicyStore, MemoryCacheClientPolicyStore>();
 builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
 builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
 
@@ -53,13 +41,19 @@ app.Use(async (context, next) =>
     var logger = context.RequestServices.GetRequiredService<ILoggerFactory>()
         .CreateLogger("OcelotGateway.Request");
 
-    logger.LogInformation("IN {Method} {Path}{Query}", context.Request.Method, 
-        context.Request.Path, context.Request.QueryString);
+    logger.LogInformation(
+        "IN {Method} {Path}{Query}",
+        context.Request.Method,
+        context.Request.Path,
+        context.Request.QueryString);
 
     await next();
 
-    logger.LogInformation("OUT {StatusCode} {Method} {Path}", context.Response.StatusCode,
-        context.Request.Method, context.Request.Path);
+    logger.LogInformation(
+        "OUT {StatusCode} {Method} {Path}",
+        context.Response.StatusCode,
+        context.Request.Method,
+        context.Request.Path);
 });
 
 app.UseIpRateLimiting();
